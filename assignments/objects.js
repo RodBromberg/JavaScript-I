ac319f3b1500762a6a915e0c3208bb26f26ad0a0
RodBromberg <rod.bromberg@yahoo.com>
2019-01-16 21:53:07 -0500

    MVP/Stretch

--- assignments/objects.js
+++ assignments/objects.js
@@ -3,39 +3,42 @@
 // ==== Challenge 1: Writing Objects ==== 
 // HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
 
+
+
+class Intern {
+    constructor(id, email, name, gender) {
+        this.id = id
+        this.email = email
+        this.name = name
+        this.gender = gender
+    }
+}
+
+
+
+// Example format of an intern object: 1,examples@you.edu,Example,F
+const example = {
+    "id": 0,
+    "name": "Example",
+    "email": "examples@you.edu",
+    "gender": "F"
+}
+
+// Write your intern objects here:
 // 1,mmelloy0@psu.edu,Mitzi,F
 // 2,kdiben1@tinypic.com,Kennan,M
 // 3,kmummery2@wikimedia.org,Keven,M
 // 4,gmartinson3@illinois.edu,Gannie,M
 // 5,adaine5@samsung.com,Antonietta,F
 
-// Example format of an intern object: 1,examples@you.edu,Example,F
-const example = {
-  "id": 0,
-  "name": "Example",
-  "email": "examples@you.edu",
-  "gender": "F"
+const mitzi = new Intern(1, 'melloy0@psu.edu', 'Mitzi', 'F');
+const antonietta = new Intern(5, 'adaine5@samsung.com', 'Antonietta', 'F')
+console.log(mitzi.name)
+
+Intern.prototype.multiply = function(a, b) {
+    return a * b
 }
-
-// Write your intern objects here:
-
-
-// ==== Challenge 2: Reading Object Data ==== 
-// Once your objects are created, log out the following requests from HR into the console:
-
-// Mitzi's name
-
-// Kennan's ID
-
-// Keven's email
-
-// Gannie's name
-
-// Antonietta's Gender
-
-// ==== Challenge 3: Object Methods ==== 
-// Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+console.log(antonietta.multiply(3, 4))
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
@@ -49,7 +52,38 @@ const example = {
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+const Parent = {
+    name: 'Susan',
+    age: 70,
+    child: {
+        name: 'George',
+        age: 50,
+        grandchild: {
+            name: 'Sam',
+            age: 30
+        }
+    }
+}
+Parent.prototype = {
+    sayName: function() {
+        return `My name is ${this.name}`
+    }
+}
+
+Parent.prototype = {
+    ageName: function() {
+        return `My name is ${this.name} and I'm ${this.age} years old`
+    }
+}
+
+
+console.log(Parent.name)
+console.log(Parent.child.age)
+console.log(Parent.prototype.ageName.call(Parent.child.grandchild))
+console.log(Parent.prototype.sayName.call(Parent))
+console.log(Parent.prototype.sayName.call(Parent.child))
+console.log(Parent.prototype.sayName.call(Parent.child.grandchild))
+
 
 // Log the parent object's name
 
